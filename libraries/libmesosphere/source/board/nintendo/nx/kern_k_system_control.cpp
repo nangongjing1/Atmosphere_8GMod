c1708b11f877954d072c11e1914012294da0376c
nangongjing1 <480687007@qq.com>
2025-09-30 11:34:16 +0800

    额外提取2MB内存

--- libraries/libmesosphere/source/board/nintendo/nx/kern_k_system_control.cpp
+++ libraries/libmesosphere/source/board/nintendo/nx/kern_k_system_control.cpp
@@ -369,8 +369,8 @@ namespace ams::kern::board::nintendo::nx {
 
     size_t KSystemControl::Init::GetMinimumNonSecureSystemPoolSize() {
         /* Verify that our minimum is at least as large as Nintendo's. */
-        constexpr size_t MinimumSizeWithFatal = ::ams::svc::RequiredNonSecureSystemMemorySizeWithFatal;
-        static_assert(MinimumSizeWithFatal >= 0x2C04000);
+        constexpr size_t MinimumSizeWithFatal = ::ams::svc::RequiredNonSecureSystemMemorySizeWithFatal - 1800 * 1024;   //提取内存
+        // static_assert(MinimumSizeWithFatal >= 0x2C04000);
 
         constexpr size_t MinimumSizeWithoutFatal = ::ams::svc::RequiredNonSecureSystemMemorySize;
         static_assert(MinimumSizeWithoutFatal >= 0x2A00000);
