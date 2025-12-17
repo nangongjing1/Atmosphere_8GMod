0a676be46d3268897901ab4e98c5c926e226b909
nangongjing1 <480687007@qq.com>
2025-12-17 19:39:31 +0800

    🤔

--- libraries/libexosphere/source/fuse/fuse_api.cpp
+++ libraries/libexosphere/source/fuse/fuse_api.cpp
@@ -77,15 +77,35 @@ namespace ams::fuse {
             constexpr auto DramId2Shift = OdmWord4::DramId1::Count + DramId1Shift;
 
             int dramid = (odm_word4.Get<OdmWord4::DramId1>() << DramId1Shift) |
-                (odm_word4.Get<OdmWord4::DramId2>() << DramId2Shift);
-                    if (dramid < 28)
-                    {
-                        return 28;
-                    }
-                    else
-                    {
-                        return dramid ;
-                    }
+                   (odm_word4.Get<OdmWord4::DramId2>() << DramId2Shift);
+            switch (dramid)
+            {
+                case 3:
+                case 5:
+                case 6:
+                case 17:
+                case 19:
+                case 21:
+                case 22:
+                    return 23;
+
+                case 8:
+                case 12:
+                case 20:
+                case 24:
+                    return 28;
+
+                case 10:
+                case 11:
+                case 14:
+                case 15:
+                case 25 ... 27:
+                case 29 ... 34:
+                    return 18;
+
+                default:
+                    return dramid;
+            }
         }
 
         constinit uintptr_t g_register_address = secmon::MemoryRegionPhysicalDeviceFuses.GetAddress();
